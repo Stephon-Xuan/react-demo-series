0a35a65ae7e11464e1f05b44ad82617882be49f4
stephon-xuan <1405028323@qq.com>
2022-10-14 10:25:12 +0800

    提交一下pageckage包形式的引入

---
+++ react-package-demo/src/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div className="App">
+          Hello
+        </div>
+      );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
